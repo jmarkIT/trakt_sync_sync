688b934d071dcefe87229f622f6df9d6cd972f00
James Mark <noemail@jamesmark.net>
2019-01-01 17:22:25 -0600

    got auth.py configured, fiddled with get_shows

--- auth.py
+++ auth.py
@@ -1,19 +1,59 @@
-import requests
-from client import client_id
+import requests, json, time
+from client import client_id, client_secret
 
-url = "https://api.trakt.tv/oauth/device/code"
+
+url = 'https://api.trakt.tv/oauth/device/code'
 
 headers = {
-    "Content-type": "application/json"
+    'Content-Type': 'applications/json'
 }
 
-parameters = {
+def device_code():
+    params = {
     "client_id": client_id
-}
+    }
 
-def main: 
-    r = requests.get(url, headers=headers, params=parameters)
-    print(r.status_code)
+    r = requests.post(url, params=params, headers=headers)
 
-if __name__ == 'main':
-    main()
+    output= json.loads(r.content)
+    device_code = output["device_code"]
+    user_code = output["user_code"]
+    verification_url = output["verification_url"]
+    print("Your usercode is {}, please go to {} to activate".format(user_code, verification_url))
+
+    return device_code
+
+
+def get_token(code):
+
+    get_token_status = ""
+
+    url = "https://api.trakt.tv/oauth/device/token"
+
+    params = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": code
+    }
+
+    while get_token_status != 200:
+        time.sleep(5)
+        
+        r = requests.post(url, params=params, headers=headers)
+        print(r.status_code)
+        get_token_status = r.status_code
+        if r.status_code == 200:
+
+            output = json.loads(r.content)
+            access_token = output["access_token"]
+            refresh_token = output["refresh_token"]
+
+            print("Your access token is {}\nand your refresh token is {}".format(access_token, refresh_token))
+            
+    
+    print(code)
+    #try:
+    #    pass
+
+if __name__ == '__main__':
+    get_token(device_code())
--- get_shows.py
+++ get_shows.py
@@ -1,19 +1,27 @@
 import requests
-from client import client_id
+from client import client_id, access_token
 
-url = "https://api.trakt.tv/shows/dukkhalatte/progress/watched"
+url = "https://api.trakt.tv/users/dukkhalatte/watched/shows"
 
 headers = {
     "Content-type": "application/json",
     "trakt-api-key": client_id,
-    "trakt-api-version": "2"
+    "trakt-api-version": "2",
+    "Authorization": "Bearer {}".format(access_token)
 }
 
-r = requests.get(url, headers=headers)
+def main(url, headers):
+    r = requests.get(url, headers=headers)
+    print(r.status_code)
+    print(client_id)
+    print(r.content)
 
-print(r.status_code)
+def get_profile(url, headers):
+    r = requests.get(url, headers=headers)
+    print(r.status_code)
+    print(client_id)
+    print(r.content)
 
-print(client_id)
-
-if __name__ == 'main':
-    main()
+if __name__ == '__main__':
+    main(url, headers)
+    get_profile(" https://api.trakt.tv/users/dukkhalatte", headers)
