52e3764805af3a55694223b083332a00409c88ac
James Mark <jmarkIT@users.noreply.github.com>
2019-04-26 19:20:26 -0500

    began updating url building structure

--- get_shows.py
+++ get_shows.py
@@ -1,7 +1,12 @@
 import requests
+import json
 from client import client_id, access_token
 
-url = "https://api.trakt.tv/users/dukkhalatte/watched/shows"
+api_url = "https://api.trakt.tv/"
+
+get_shows_url = "sync/watched/shows"
+
+get_progress_url = "https://api.trakt.tv/shows/game-of-thrones/progress/watched"
 
 headers = {
     "Content-type": "application/json",
@@ -12,9 +17,20 @@ headers = {
 
 def get_shows(url, headers):
     r = requests.get(url, headers=headers)
-    print(r.status_code)
-    print(client_id)
-    print(r.content)
+    #print(r.status_code)
+    #print(client_id)
+    #print(json.loads(r.content))
+    #return json.loads(r.content))
+    return r.content
+    
+def get_progress(url, headers):
+		r = requests.get(url, headers=headers)
+		return r.json()
 
 if __name__ == '__main__':
-    get_shows(url, headers)
+    #get_shows(url, headers)
+    #show_sync_data = get_shows(get_shows_url, headers)
+    
+    show_progress_data = get_progress(get_progress_url, headers)
+    
+    print(show_progress_data)
