390ddd8762678fdc4881feb075e0d76826d9121a
James Mark <james.david.mark@gmail.com>
2019-04-27 15:14:47 -0500

    Created main statement to test database output

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
 import sqlite3
 from sqlite3 import Error
 
+sqlite_file = "trakt_shows.sqlite"
+table_name = "shows"
+id_column = "show_name"
+field_type = "TEXT"
+
+
 def create_connection(db_file):
 	""" create a database connection to the SQLite database """
 	try:
 		conn = sqlite3.connect(db_file)
-		print(sqlite3.version)
 	except Error as e:
 		print(e)
 	
-	return None
+	return conn
 	
 
 def create_table(conn, create_table_sql):
@@ -23,3 +28,12 @@ def create_table(conn, create_table_sql):
 		c.execute(create_table_sql)
 	except Error as e:
 		print(e)
+		
+		
+if __name__ == "__main__":
+	create_table_sql = f'CREATE TABLE {table_name} ({id_column} {field_type} PRIMARY KEY)'
+	
+	conn = create_connection(sqlite_file)
+	
+	create_table(conn, create_table_sql) 
+
