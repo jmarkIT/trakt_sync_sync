eb0ec8c6e72653d1562589d968729596888eb455
James Mark <james.david.mark@gmail.com>
2019-05-06 12:31:31 -0500

    Updated get_shows to return the slug of all watched shows

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
 import json
+import database
 from client import client_id, access_token
+
 
 api_url = "https://api.trakt.tv/"
 
 get_shows_url = "sync/watched/shows"
 
-get_progress_url = "https://api.trakt.tv/shows/game-of-thrones/progress/watched"
+get_progress_url = "https://api.trakt.tv/shows/legion/progress/watched"
 
 headers = {
     "Content-type": "application/json",
@@ -18,15 +20,40 @@ headers = {
 
 def get_shows(url, headers):
     r = requests.get(url, headers=headers)
-    return r.content
+    
+    show_list = []
+    shows = r.json()
+    
+    #print(shows)
+    
+    for show in shows:
+    	if show["show"]["ids"]["slug"] not in show_list:
+    		show_list.append(show["show"]["ids"]["slug"])
+    
+    
+    return show_list	
 
 
 def get_progress(url, headers):
     r = requests.get(url, headers=headers)
-    return r.json()
+    show_progress = r.json()
+    print(len(show_progress['seasons']))
+    
+    seasons = show_progress['seasons']
+    
+    for season in seasons:
+    	season_number = season['number']
+    	
+    	episodes = season['episodes']
+    	
+    	print(episodes)
 
 
 if __name__ == '__main__':
-    show_progress_data = get_progress(get_progress_url, headers)
+    #show_progress_data = get_progress(get_progress_url, headers)
+    
+    show_sync_data = get_shows("https://api.trakt.tv/sync/watched/shows", headers)
 
-    print(show_progress_data)
+    #print(show_progress_data)
+    for title in show_sync_data:
+    	print(title)
